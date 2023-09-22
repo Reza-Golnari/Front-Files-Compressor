3bd45e75158554ba936ed09c0347feb9f0cc78bc
Reza <i.am.reza.programmer@gmail.com>
2023-09-22 11:11:25 +0330

    Add routes

---
+++ assest/js/router/router.js
@@ -0,0 +1,12 @@
+const mainTitle = "File Compressor";
+
+const router = {
+  "/": {
+    template: "./index.html",
+    title: `${mainTitle}`,
+  },
+  "/about": {
+    template: "./about.html",
+    title: `${mainTitle} | About`,
+  },
+};
