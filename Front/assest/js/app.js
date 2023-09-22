30e032694d5b5336a6a75e46d4bf470944d0e222
Reza <i.am.reza.programmer@gmail.com>
2023-09-22 22:38:58 +0330

    Download compressed file completed

--- Front/assest/js/app.js
+++ Front/assest/js/app.js
@@ -5,6 +5,9 @@ const fileInput = $.querySelector(".file-input");
 const uploadBox = $.querySelector(".main__upload-box__container");
 const haveFileBox = $.querySelector(".have-file");
 const noFileBox = $.querySelector(".no-file");
+const uploadProgress = $.querySelector(".upload-progress");
+const responseProgress = $.querySelector(".response-progress");
+const downloadBtn = $.querySelector(".download-btn");
 
 $.addEventListener("drop", (e) => {
   e.preventDefault();
@@ -49,13 +52,20 @@ async function sendFile(file) {
   await axios
     .post("http://127.0.0.1:8000/compress", formData, {
       onUploadProgress: function (progressEvent) {
-        const percentCompleted = Math.round(
+        const progress = Math.round(
           (progressEvent.loaded * 100) / progressEvent.total
         );
       },
     })
     .then(function (response) {
       console.log(response);
+      let mainFile = window.URL.createObjectURL(new Blob([response.data]));
+      //   mainFile = mainFile.slice(5, -1);
+      downloadBtn.href = mainFile;
+      downloadBtn.setAttribute("download", "vue.js");
+      downloadBtn.classList.add("show");
+      console.log(mainFile);
+      console.log(downloadBtn);
     })
     .catch(function (error) {
       console.log(error);
